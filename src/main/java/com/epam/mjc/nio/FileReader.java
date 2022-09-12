404e9cc38fe53d08aee60f109d98cb851417d18d
Bilbo3340 <vladik.elce1@gmail.com>
2022-09-13 00:51:36 +0300

    upt

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,36 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
+import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        if (file == null) {
+            return new Profile();
+        }
+        StringBuilder def = new StringBuilder();
+        try (java.io.FileReader fr = new java.io.FileReader(file))
+        {
+            int content;
+            while ((content = fr.read()) != -1) {
+                if (content != 13) {
+                    def.append((char) content);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        def = new StringBuilder(def.toString().replace("Name: ", ""));
+        def = new StringBuilder(def.toString().replace("Age: ", ""));
+        def = new StringBuilder(def.toString().replace("Email: ", ""));
+        def = new StringBuilder(def.toString().replace("Phone: ", ""));
+        String[] result = def.toString().split("\n");
+        return new Profile(result[0], Integer.valueOf(result[1]), result[2], Long.valueOf(result[3]));
+    }
+
+    public static void main(String[] args) {
+        FileReader f = new FileReader();
+        f.getDataFromFile(new File("src/main/resources/Profile.txt"));
     }
 }
